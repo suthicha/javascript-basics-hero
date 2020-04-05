7ac8be507447edad7a7323e330d5bf42591207e8
suthicha <suthicha@outlook.com>
2020-04-05 13:06:32 +0700

    final variables and data type

--- js-variables-data-type/script.js
+++ js-variables-data-type/script.js
@@ -1,8 +1,48 @@
+// var name = 'Suthicha';
+// console.log(name);
+
+// var lastName = 'Poonakaow';
+// console.log(lastName);
+
+// var age = 30;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// Comment: combine variables
 var name = 'Suthicha';
-console.log(name);
+var age = 30
 
-var lastName = 'Poonakaow';
-console.log(lastName);
+console.log(name + age);
+console.log(age + age);
 
-var age = 30;
-console.log(age);
+var job, isMarried;
+console.log(job);
+
+job = 'Programmer';
+isMarried = false;
+
+console.log(
+    name + ' is a ' + 
+    age + ' years old ' + 
+    job + '. Is he merried? ' + 
+    isMarried + '.');
+
+age = 'thirty';
+job = 'driver'
+console.log(
+    name + ' is a ' + 
+    age + ' years old ' + 
+    job + '. Is he merried? ' + 
+    isMarried + '.');
+
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+alert(
+    name + ' is a ' + 
+    age + ' years old ' + 
+    job + '. Is he merried? ' + 
+    isMarried + '.'
+);
