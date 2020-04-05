87a2b51351dc8b535dfa848182a1ac4309c921da
suthicha <suthicha@outlook.com>
2020-04-05 13:33:19 +0700

    add basic operators

---
+++ js-operators/script.js
@@ -0,0 +1,19 @@
+var year = 2020;
+var birthYear = year - 30;
+console.log(birthYear);
+
+birthYear = (year - 30) * 2;
+console.log(birthYear);
+
+var ageAke = 30;
+var ageGorge = 30;
+
+ageAke = ageGorge = (3 + 5) * 4 - 6;
+
+ageGorge++;
+
+ageAke *= 2;
+ageAke = ageAke * 2;
+
+console.log('Gorge : ' + ageGorge)
+console.log('Ake : ' + ageAke);
